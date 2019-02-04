47dcc6cde6ce31e851d53129277ed4da1e13aa5d
darutoko <darutoko@yandex.ru>
2019-02-04 15:43:58 +0700

    see prev commit

--- core/app.js
+++ core/app.js
@@ -8,7 +8,7 @@ let schema = require("./graphql");
 
 let app = express();
 
-app.use((req, res, next) => { console.log(`${req.method} ${req.originalUrl}`); next() });
+if (process.env.NODE_LOG) app.use((req, res, next) => { console.log(`${req.method} ${req.originalUrl}`); next() });
 app.use(bodyParser.json());
 app.use(express.static('public'));
 app.use("/api", graphqlHTTP({
