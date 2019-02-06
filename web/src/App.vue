e28877b522b4fd309453f4e441b47c49b38b57f8
darutoko <darutoko@yandex.ru>
2019-02-06 17:51:31 +0700

    add Apollo Client

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,38 +1,63 @@
 <template>
   <v-app>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
+    <v-navigation-drawer app clipped v-model="drawer.isShown">
+      <v-list>
+        <v-list-tile v-for="item in drawer.items" :key="item.name" :to="item.route">
+          <v-list-tile-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>{{ item.name }}</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-toolbar app clipped-left dark color="blue darken-3">
+      <v-toolbar-side-icon @click="toggleDrawer"></v-toolbar-side-icon>
+      <v-toolbar-title>HMWA</v-toolbar-title>
     </v-toolbar>
 
     <v-content>
-      <HelloWorld/>
+      <v-container>
+        <router-view></router-view>
+      </v-container>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  },
-  data () {
+  name: "App",
+  components: {},
+  data() {
     return {
-      //
+      drawer: {
+        isShown: null,
+        items: [
+          {
+            icon: "live_tv",
+            name: "Сериалы",
+            route: "/"
+          },
+          {
+            icon: "restaurant_menu",
+            name: "Еда",
+            route: "/food"
+          },
+          {
+            icon: "video_label",
+            name: "Видео",
+            route: "/video"
+          }
+        ]
+      }
+    };
+  },
+  methods: {
+    toggleDrawer() {
+      this.drawer.isShown = !this.drawer.isShown;
     }
   }
-}
+};
 </script>
