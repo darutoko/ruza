853440ab83923496702aaf2ff9655fc9b52648f8
darutoko <darutoko@yandex.ru>
2019-02-06 17:50:56 +0700

    add scaffold for Food page

---
+++ web/src/views/Food/Food.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-layout>
+    <v-flex>Food</v-flex>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  name: "Food",
+  data() {
+    return {
+      //
+    };
+  },
+  created() {
+    this.fetchData();
+  },
+  methods: {
+    fetchData() {
+      //
+    }
+  }
+};
+</script>
