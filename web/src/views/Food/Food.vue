a1679c62872813fbbf07980f172b571e6e573f5e
darutoko <darutoko@yandex.ru>
2019-02-08 17:58:43 +0700

    add food main page

--- web/src/views/Food/Food.vue
+++ web/src/views/Food/Food.vue
@@ -1,23 +1,120 @@
 <template>
   <v-layout>
-    <v-flex>Food</v-flex>
+    <v-flex>
+      <v-expansion-panel>
+        <v-expansion-panel-content>
+          <div slot="header">Ингредиенты</div>
+
+          <v-card>
+            <v-card-text>
+              <v-layout>
+                <v-flex class="text-xs-center">
+                  <v-btn class="mx-0" color="primary" small @click="filterAll">Все</v-btn>
+                  <v-btn class="mx-0" color="primary" small @click="filterNone">Очистить</v-btn>
+                  <v-btn class="mx-0" color="primary" small @click="filterInvert">Инверт.</v-btn>
+                </v-flex>
+              </v-layout>
+
+              <v-layout wrap>
+                <v-flex v-for="ingredient in ingredients" :key="ingredient.name">
+                  <v-checkbox v-model="filter" :label="ingredient.name" :value="ingredient.name"></v-checkbox>
+                </v-flex>
+              </v-layout>
+            </v-card-text>
+          </v-card>
+        </v-expansion-panel-content>
+      </v-expansion-panel>
+
+      <v-tabs fixed-tabs>
+        <v-tab v-for="type in types" :key="type.id">{{ type.name }}</v-tab>
+
+        <v-tab-item v-for="type in types" :key="type.id">
+          <v-card v-for="dish in filteredDishes(type.dishes)" :key="dish.id">
+            <v-card-title class="title blue darken-2 white--text">{{ dish.name }}</v-card-title>
+
+            <v-card-text>
+              <v-layout column>
+                <v-flex
+                  v-for="ingredient in dish.ingredients"
+                  :key="ingredient.id"
+                  class="body-2"
+                >{{ ingredient.name }}</v-flex>
+                <v-flex
+                  v-if="dish.recipe"
+                  class="pt-2"
+                  style="white-space: pre-wrap;"
+                >{{ dish.recipe }}</v-flex>
+              </v-layout>
+            </v-card-text>
+          </v-card>
+        </v-tab-item>
+      </v-tabs>
+    </v-flex>
   </v-layout>
 </template>
 
 <script>
+import gql from "graphql-tag";
+
 export default {
   name: "Food",
   data() {
     return {
-      //
+      types: [],
+      ingredients: [],
+      filter: []
     };
   },
-  created() {
-    this.fetchData();
+  apollo: {
+    types: {
+      query: gql`
+        {
+          types {
+            id
+            name
+            dishes {
+              id
+              name
+              recipe
+              ingredients {
+                id
+                name
+              }
+            }
+          }
+        }
+      `
+    },
+    ingredients: {
+      query: gql`
+        {
+          ingredients {
+            id
+            name
+          }
+        }
+      `
+    }
   },
   methods: {
-    fetchData() {
-      //
+    filterAll() {
+      this.filter = this.ingredients.map(ingredient => ingredient.name);
+    },
+    filterNone() {
+      this.filter = [];
+    },
+    filterInvert() {
+      this.filter = this.ingredients
+        .map(ingredient => ingredient.name)
+        .filter(ingredient => !this.filter.includes(ingredient));
+    },
+    filteredDishes(dishes) {
+      return dishes.filter(dish => {
+        for (let ingredient of dish.ingredients) {
+          if (this.filter.includes(ingredient.name)) return true;
+        }
+        return false;
+      });
     }
   }
 };
--- web/src/vue-apollo.js
+++ web/src/vue-apollo.js
@@ -65,6 +65,10 @@ export function createProvider(options = {}) {
 		},
 		errorHandler(error) {
 			// eslint-disable-next-line no-console
+			this.$store.commit("addAlert", {
+				type: "error",
+				message: error.message
+			});
 			console.log('%cError', 'background: red; color: white; padding: 2px 4px; border-radius: 3px; font-weight: bold;', error.message)
 		},
 	})
