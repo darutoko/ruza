a86738c31fd966675118f3cca98ccdfd6082d918
darutoko <darutoko@yandex.ru>
2019-03-12 15:50:58 +0700

    add plugin to handle graphql requests

---
+++ web/src/plugins/graphql-query.js
@@ -0,0 +1,40 @@
+import Vue from "vue";
+import gql from "graphql-tag";
+
+let GraphQLQuery = {
+	install(Vue) {
+		Vue.mixin({
+			methods: {
+				async graphql(request, callback) {
+					let loading;
+					if (request.loadingKey) loading = (isLoading) => this[request.loadingKey].loading = isLoading;
+					else loading = (isLoading) => this.$store.commit("loading", isLoading);
+
+					loading(true);
+					try {
+						let result;
+						if (request.query) {
+							result = await this.$apollo.query({
+								query: gql(request.query),
+								variables: request.variables
+							});
+						} else if (request.mutation) {
+							result = await this.$apollo.mutate({
+								mutation: gql(request.mutation),
+								variables: request.variables
+							});
+						} else {
+							throw "Unknown query type";
+						}
+						callback(result.data);
+					} catch (error) {
+						this.$store.commit("error", error.message);
+					}
+					loading(false);
+				}
+			}
+		});
+	}
+};
+
+Vue.use(GraphQLQuery);
