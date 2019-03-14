411ee652403ef81db43a36007fdae931c3c8cd61
darutoko <darutoko@yandex.ru>
2019-03-14 14:48:05 +0700

    refactor dish page

--- web/src/views/Food/Dish.vue
+++ web/src/views/Food/Dish.vue
@@ -1,45 +1,55 @@
 <template>
-  <v-layout>
-    <v-flex>
-      <v-form ref="form">
-        <v-text-field v-model.trim="form.name" label="Название" :rules="stringRules" required></v-text-field>
+	<v-form ref="form" @submit.prevent="saveDish">
+		<v-layout column>
+			<v-flex>
+        <v-text-field v-model.trim="form.name" label="Название" :rules="textRules" required></v-text-field>
+			</v-flex>
+			<v-flex>
         <v-select
           v-model="form.typeId"
           :items="types"
-          :rules="numberRules"
           item-text="name"
           item-value="id"
           label="Вид"
-          required
-        ></v-select>
+          required></v-select>
+			</v-flex>
+			<v-flex>
         <v-select
           v-model="form.ingredients"
           :items="ingredients"
-          :rules="arrayRules"
+          :rules="multiSelectRules"
           item-text="name"
           item-value="id"
           label="Ингредиенты"
-          multiple
-          chips
-          required
-        ></v-select>
+          multiple chips required></v-select>
+			</v-flex>
+			<v-flex>
         <v-textarea v-model="form.recipe" label="Рецепт"></v-textarea>
-        <v-btn color="primary" outline :to="{ name: 'food_list' }">
-          <v-icon>navigate_before</v-icon>Назад
-        </v-btn>
-        <v-btn color="warning" :disabled="form.loading" @click.prevent="resetForm">Сброосить</v-btn>
-        <v-btn
-          color="success"
-          :loading="form.loading"
-          :disabled="form.loading"
-          @click.prevent="saveDish"
-        >Сохранить</v-btn>
-      </v-form>
-    </v-flex>
-  </v-layout>
+			</v-flex>
+			<v-flex>
+				<v-layout wrap>
+					<v-flex xs12 sm6>
+						<v-btn color="primary" outline :to="{ name: 'food_list' }">
+							<v-icon>navigate_before</v-icon>Назад
+						</v-btn>
+					</v-flex>
+					<v-flex xs5 sm6	class="text-sm-right text-xs-left">
+						<v-btn color="warning" :disabled="form.loading" @click.prevent="resetForm">Сброосить</v-btn>
+						<v-btn
+							type="submit"
+							color="success"
+							:loading="form.loading"
+							:disabled="form.loading">Сохранить</v-btn>
+					</v-flex>
+				</v-layout>
+			</v-flex>
+		</v-layout>
+	</v-form>
 </template>
 
 <script>
+import { textRequired } from "@/utils/rules.js";
+
 export default {
   name: "Dish",
   data() {
@@ -59,133 +69,66 @@ export default {
         ingredients: [],
         recipe: ""
       },
-      stringRules: [this.stringRequired],
-      numberRules: [this.numberRequired],
-      arrayRules: [this.arrayOneOrMore]
+			textRules: [textRequired],
+      multiSelectRules: [this.multiSelectOneOrMore]
     };
   },
   methods: {
-    async fetchData() {
+    fetchData() {
       let prefix = "{";
-      if (this.$route.params.id) prefix = `query($id: Int!) {
-				dish(id: $id) {
-					id
-					name
-					typeId
-					recipe
-					ingredients {
-						id
-					}
-				}`;
+      if (this.$route.params.id) prefix = `query($id: Int!) { dish(id: $id) { id name typeId recipe ingredients { id } }`;
 
-			this.$store.commit("loadingStart");
-			try {
-				let result = await this.$apollo.query({
-						query: this.$gql`${prefix}
-							types {
-								id
-								name
-							}
-							ingredients {
-								id
-								name
-							}
-						}`,
-						variables: {
-							id: this.$route.params.id
-						}
-				});
-
-				if (this.$route.params.id) this.dish = result.data.dish;
-				this.types = result.data.types;
-				this.ingredients = result.data.ingredients;
-
+			this.graphql({
+				query: `${prefix} types { id name } ingredients { id name } }`,
+				variables: {
+					id: this.$route.params.id
+				},
+				fetchPolicy: "no-cache",
+			},
+			data => {
+				if (this.$route.params.id) this.dish = data.dish;
+				this.types = data.types;
+				this.ingredients = data.ingredients;
 				this.resetForm();
-			} catch (error) {
-				console.log(error);
-			}
-			this.$store.commit("loadingStop");
+			});
     },
-    async saveDish() {
+    saveDish() {
       if (!this.$refs.form.validate()) return;
 
-      this.form.loading = true;
-      if (this.$route.params.id) {
-        await this.updateDish();
-      } else {
-        await this.addDish();
-      }
-      this.form.loading = false;
+      if (this.$route.params.id) this.updateDish()
+			else this.addDish();
     },
-    async addDish() {
-      try {
-        let result = await this.$apollo.mutate({
-          mutation: this.$gql`
-						mutation($name: String!, $typeId: Int!, $recipe: String, $ingredients: [Int!]) {
-							addDish(name: $name, typeId: $typeId, recipe: $recipe, ingredients: $ingredients)
-						}`,
-          variables: {
-            ...this.form
-          }
-				});
-
-        this.$store.commit("addAlert", {
-          type: "success",
-          message: `Блюдо ${this.form.name} добавлено с ID ${result.data.addDish}`
-				});
+    addDish() {
+			this.graphql({
+				mutation: "mutation($name: String!, $typeId: Int!, $recipe: String, $ingredients: [Int!]) { addDish(name: $name, typeId: $typeId, recipe: $recipe, ingredients: $ingredients) }",
+				variables: { ...this.form },
+				loadingKey: "form",
+			},
+			data => {
 				this.resetForm();
-      } catch (error) {
-        this.$store.commit("addAlert", {
-          type: "error",
-          message: error.message
-        });
-      }
+				this.$store.commit("success", `Блюдо ${this.form.name} добавлено с ID ${data.addDish}`);
+			});
     },
-    async updateDish() {
-      try {
-        let result = await this.$apollo.mutate({
-          mutation: this.$gql`
-						mutation($id: Int!, $name: String!, $typeId: Int!, $recipe: String, $ingredients: [Int!]) {
-							updateDish(id: $id, name: $name, typeId: $typeId, recipe: $recipe, ingredients: $ingredients)
-						}`,
-          variables: {
-            ...this.form,
-            id: this.$route.params.id
-          }
-        });
-
-        if (result.data.updateDish) {
-          this.$store.commit("addAlert", {
-            type: "success",
-            message: `Блюдо ${this.form.name} обновлено`
-          });
-          this.$router.replace({ name: "food_list" });
-        }
-      } catch (error) {
-        this.$store.commit("addAlert", {
-          type: "error",
-          message: error.message
-        });
-      }
+    updateDish() {
+			this.graphql({
+				mutation: "mutation($id: Int!, $name: String!, $typeId: Int!, $recipe: String, $ingredients: [Int!]) { updateDish(id: $id, name: $name, typeId: $typeId, recipe: $recipe, ingredients: $ingredients)}",
+				variables: { ...this.form, id: this.$route.params.id },
+				loadingKey: "form",
+			},
+			data => {
+				this.resetForm();
+				this.$store.commit("success", `Блюдо ${this.form.name} обновлено`);
+				this.$router.replace({ name: "food_list" });
+			});
     },
     resetForm() {
-      let form = this.form,
-        dish = this.dish;
-      form.name = dish.name;
-      form.typeId = dish.typeId;
-      form.ingredients = dish.ingredients.map(ingredient => ingredient.id);
-      form.recipe = dish.recipe;
+      this.form.name = this.dish.name;
+      this.form.typeId = this.dish.typeId || this.types[0].id;
+      this.form.ingredients = this.dish.ingredients.map(ingredient => ingredient.id);
+      this.form.recipe = this.dish.recipe;
       this.$refs.form.resetValidation();
     },
-    stringRequired(value) {
-      if (value) return true;
-      return "Поле необходимо заполнить";
-    },
-    numberRequired(value) {
-      if (value) return true;
-      return "Поле необходимо заполнить";
-    },
-    arrayOneOrMore(value) {
+    multiSelectOneOrMore(value) {
       if (value.length > 0) return true;
       return "Необходимо выбрать хотя бы один элемент";
     }
