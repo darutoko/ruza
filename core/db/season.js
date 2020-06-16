899e2f3a69fed30b1893671f644a0d61d0497083
darutoko <darutoko@gmail.com>
2020-06-16 19:12:50 +0700

    rename field

--- core/db/season.js
+++ core/db/season.js
@@ -1,7 +1,7 @@
 let pg = require("./pg")
 
 module.exports = {
-	list({ show_id }) {
-		return pg.query("select * from season where show_id = $1;", [show_id])
+	list({ showId }) {
+		return pg.query('select * from "season" where "showId" = $1;', [showId])
 	},
 }
--- core/graphql/tools.js
+++ core/graphql/tools.js
@@ -2,7 +2,4 @@ module.exports = {
 	checkAuthorization(req) {
 		if (!req.user.isAdmin) throw new Error("Access denied")
 	},
-	rejectedPromise(reason) {
-		return Promise.reject(new Error(reason))
-	},
 }
