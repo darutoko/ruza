0c5e65db21d89bd57cb8fb1016ebe8d9010c3aa3
darutoko <darutoko@gmail.com>
2020-07-10 20:03:42 +0700

    pretty app.js

--- core/app.js
+++ core/app.js
@@ -1,28 +1,40 @@
-process.chdir(__dirname);
-require("./config.js");
+process.chdir(__dirname)
+require("./config.js")
 
-let express = require("express");
-let bodyParser = require("body-parser");
-let graphqlHTTP = require("express-graphql");
+let express = require("express")
+let bodyParser = require("body-parser")
+let graphqlHTTP = require("express-graphql")
 
-let schema = require("./graphql");
-let auth = require("./middleware/auth.js");
+let schema = require("./graphql")
+let auth = require("./middleware/auth.js")
 
-let app = express();
+let app = express()
 
-// app.use((req, res, next) => { setTimeout(() => next(), 2000) });
+// app.use((req, res, next) => { setTimeout(() => next(), 1000) })
 // TODO:
 
-app.use(bodyParser.json());
-if (process.env.NODE_ENV !== "production") app.use((req, res, next) => { console.log(`${req.method} ${req.originalUrl}`); console.log(req.body); next(); });
-app.use(express.static('public'));
-app.use("/api", auth(schema));
-app.use("/api", graphqlHTTP({
-	schema,
-	graphiql: process.env.NODE_ENV !== "production"
-}));
-app.use("/*", (req, res, next) => {
-	res.sendFile(__dirname + '/public/index.html');
-});
+app.use(bodyParser.json())
 
-app.listen(process.env.PORT, () => console.log(`Listening at port ${process.env.PORT}`));
+if (process.env.NODE_ENV !== "production")
+	app.use((req, res, next) => {
+		console.log(`${req.method} ${req.originalUrl}`)
+		console.log(req.body)
+		next()
+	})
+
+app.use(express.static("public"))
+app.use("/api", auth(schema))
+
+app.use(
+	"/api",
+	graphqlHTTP({
+		schema,
+		graphiql: process.env.NODE_ENV !== "production",
+	})
+)
+
+app.use("/*", (req, res, next) => {
+	res.sendFile(__dirname + "/public/index.html")
+})
+
+app.listen(process.env.PORT, () => console.log(`Listening at port ${process.env.PORT}`))
